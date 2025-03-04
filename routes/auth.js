592a02cd54ce89b4a107ec5b361943136fecab39
Amaldeep <amaldeeppatra21@gmail.com>
2025-03-05 01:33:35 +0530

    temp changes

--- routes/auth.js
+++ routes/auth.js
@@ -14,24 +14,25 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    const token = createTokenForUser(req.user);
+    // const token = createTokenForUser(req.user);
     // Set the JWT as an HttpOnly cookie so it's not accessible via JavaScript.
-    res.cookie('auth_token', token, {
+    // res.cookie('auth_token', token, {
       // httpOnly: true, // Prevents JS access (helps mitigate XSS)
       // secure: process.env.NODE_ENV, // Only send cookie over HTTPS in production
       // sameSite: 'strict',
-      maxAge: 1 * 60 * 60 * 1000, // 7 hours in milliseconds
-    });
+    //   maxAge: 1 * 60 * 60 * 1000, // 7 hours in milliseconds
+    // });
     
     // Check if the user already has a phone number (i.e. is an existing user).
     // Adjust this condition based on your user schema (e.g., using a field like "phone" or "isNewUser")
-    if (!req.user.email) {
-      // New user: redirect to the page for entering phone number.
-      res.redirect(`${process.env.VITE_APP_URL}/login-phone`);
-    } else {
-      // Existing user: redirect to the homepage.
-      res.redirect(`${process.env.VITE_APP_URL}/homepage`);
-    }
+    // if (!req.user.email) {
+    //   // New user: redirect to the page for entering phone number.
+    //   res.redirect(`${process.env.VITE_APP_URL}/login-phone`);
+    // } else {
+    //   // Existing user: redirect to the homepage.
+    //   res.redirect(`${process.env.VITE_APP_URL}/homepage`);
+    // }
+    res.redirect(`${process.env.VITE_APP_URL}/homepage`);
   }
 );
 

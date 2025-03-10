782d3a13cbdad7c13aff0a63d60a457bd890f41e
Amaldeep <amaldeeppatra21@gmail.com>
2025-03-10 18:51:24 +0530

    temp

--- index.js
+++ index.js
@@ -19,8 +19,25 @@ const getProdByCat = require('./routes/getProduct');
 const searchRoutes = require('./routes/search');
 
 // app.use(cors({ origin: ['http://10.2.99.211:5173', 'http://localhost:5173', 'https://millet-kiosk-app.vercel.app', 'http://10.2.105.237:5173'], credentials: true }));
-app.use(cors({ origin: "*", credentials: true }));
-// app.use(cors({ credentials: true }));
+// app.use(cors({ origin: ["https://millet-kiosk-app.vercel.app", "http://localhost:5173"], credentials: true }));
+const allowedOrigins = [
+  "https://millet-kiosk-app.vercel.app",
+  "http://localhost:5173"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());

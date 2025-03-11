a268d8916c9b110a3ebdf1ef8c7dfe4594ab9ff6
Amaldeep <amaldeeppatra21@gmail.com>
2025-03-11 22:49:43 +0530

    temp

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  // res.header("Access-Control-Allow-Origin", "https://millet-kiosk-app.vercel.app"); // No wildcard!
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // No wildcard!
+  res.header("Access-Control-Allow-Origin", "https://millet-kiosk-app.vercel.app"); // No wildcard!
+  // res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // No wildcard!
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
